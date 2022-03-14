4eeecc9708e9283404b9b9fb3d3011fafe8b6992
yahia-abrini <77554738+yahia-abrini@users.noreply.github.com>
2022-03-14 12:42:45 -0600

    Added Basic Demo Code (copied from Zaid)

---
+++ basicdemo/basicdemo.ino
@@ -0,0 +1,50 @@
+#define timeSeconds 10
+
+const int led = 13;
+const int motionSensor = 14;
+
+// Timer: Auxiliary variables
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+
+void IRAM_ATTR detectsMovement() 
+{
+  Serial.println("MOTION DETECTED!!!");
+  digitalWrite(led, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+}
+
+void setup() 
+{
+   Serial.begin(115200);
+   // PIR Motion Sensor mode INPUT_PULLUP
+  pinMode(motionSensor, INPUT_PULLUP);
+  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+
+  // Set LED to LOW
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+}
+
+unsigned long then = 0;
+void loop() 
+{
+   now = millis();
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if(startTimer && (now - lastTrigger > (timeSeconds*1000))) 
+  {
+    Serial.println("Motion stopped...");
+    digitalWrite(led, LOW);
+    startTimer = false;
+  }
+
+  if (now > then + 2000)
+  {
+    then = now;
+    Serial.println("It is working...");
+  }
+  
+}
