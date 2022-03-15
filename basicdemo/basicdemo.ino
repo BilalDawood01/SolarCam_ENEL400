476f3eacbf94f7283fc7ab6e7898e3f171c23add
yahia-abrini <77554738+yahia-abrini@users.noreply.github.com>
2022-03-15 08:45:20 -0600

    Outputs Message when PIR Activates, keeps doing so, then Deep Sleep

--- basicdemo/basicdemo.ino
+++ basicdemo/basicdemo.ino
@@ -1,10 +1,10 @@
 
 // Define minimal time to record after every sensor activation 
-#define timeSeconds 15
+#define timeSeconds 5
 
-/* ----- ----- SET MOTION SENSOR INPUT PIN AND LED OUTPUT PIN VALUES ----- ----- */
-const int led = 13;               
-const int motionSensor = 14;
+/* ----- ----- SET MOTION SENSOR INPUT PIN AND LEDPin OUTPUT PIN VALUES ----- ----- */
+const int LEDPin = 13;               
+const int MSPin = 14;
 
 // Timer: Auxiliary variables
 unsigned long now = millis();
@@ -14,64 +14,61 @@ boolean startTimer = false;
 
 
 /* ----- ----- INTERRUPT SERVICE ROUTINE TRIGGERED BY PIR SENSOR ----- ----- */
-void IRAM_ATTR detectsMovement() 
-{
-  if(startTimer == false){
-    Serial.println("MOTION DETECTED!!!");
-    // print message to serial monitor
-    
-    digitalWrite(led, HIGH);
-    // Set LED pin to HIGH
-    
-    startTimer = true;
-    
-    lastTrigger = millis();
-  }
-}
+//void IRAM_ATTR detectsMovement() 
+//{
+//    Serial.println("ISR Ran!");
+//    if(!digitalRead(MSPin) && startTimer == false){
+//      Serial.println("1st");
+//      lastTrigger = millis();
+//      
+//      digitalWrite(LEDPin, HIGH);
+//      // Set LEDPin pin to HIGH
+//      
+//      startTimer = true;
+//    } else if (digitalRead(MSPin) && startTimer == true){
+//      Serial.println("2nd");
+//      digitalWrite(LEDPin, LOW);
+//      startTimer = false;
+//    } else {
+//      Serial.println("3rd");
+//    }
+//}
 
 
 /* ----- ----- INITIALIZATION ----- ----- */
 void setup() 
 {
+  Serial.println("Woke up");
+  // SETTING UP SERIAL COMMUNICATION
   Serial.begin(115200);          
   // Sets the data rate in bits per second (baud) for serial data transmission.
 
   // SETTING UP PINS
-  pinMode(motionSensor, INPUT_PULLUP);
+  pinMode(MSPin, INPUT_PULLUP);
   // Set PIR Motion Sensor mode to INPUT_PULLUP
-  pinMode(led, OUTPUT);
-  // Set LED pin as an output
-  digitalWrite(led, LOW);
-  // Set LED pin to LOW
+  pinMode(LEDPin, OUTPUT);
+  // Set LEDPin pin as an output
+  digitalWrite(LEDPin, LOW);
+  // Set LEDPin pin to LOW
 
+    while(digitalRead(MSPin)){
+      Serial.println("DigitalRead High"); 
+      delay(2000);
+    }
+  
+    Serial.println("DigitalRead Low");
   // MOTION SENSOR INTERRUPT
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-  // Set motionSensor pin as interrupt, assign interrupt function "detectsMovement" to run when activated, and set RISING mode
+//  attachInterrupt(digitalPinToInterrupt(MSPin), detectsMovement, CHANGE);
+  // Set MSPin pin as interrupt, assign interrupt function "detectsMovement" to run when activated, and set RISING mode
+    Serial.println("Going to Sleep...");
+    delay(1000);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_14,1);
+    esp_deep_sleep_start();
 }
 
 /* ----- ----- MAIN PROGRAM - CONTINUOUS LOOP ----- ----- */
 void loop() 
 {
-  
-  // Turn off the LED after the number of seconds defined in the timeSeconds variable
-  now = millis();
-  if(startTimer){
-      if((now - lastTrigger > (timeSeconds*1000))){
-      if(digitalRead(motionSensor)){
-        lastTrigger = millis();
-      } else {
-        Serial.println("Motion stopped...");
-        digitalWrite(led, LOW);
-        startTimer = false;
-      }
-    }
-  }
+ 
 
-//  // FOR TESTING PURPOSES: GIVE MESSAGE EVERY 2 SECONDS INDICATED ESP32 IS ON
-//  if (now > then + 2000)
-//  {
-//    then = now;
-//    Serial.println("It is working...");
-//  }
-  
 }
