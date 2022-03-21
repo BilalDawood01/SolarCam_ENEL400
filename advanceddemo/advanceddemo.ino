650c7fde764eb93ce4a4a4fbde2b9c8eea7d2a01
yahia-abrini <77554738+yahia-abrini@users.noreply.github.com>
2022-03-21 10:21:10 -0600

    Created File, Setting Up Code

---
+++ advanceddemo/advanceddemo.ino
@@ -0,0 +1,11 @@
+void setup() 
+{
+  // Initialize the Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void loop() {}
