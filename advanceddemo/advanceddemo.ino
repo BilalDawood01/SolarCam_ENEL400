5d8bc98e7e8353f7c84ed253d803a6800c74a140
yahia-abrini <77554738+yahia-abrini@users.noreply.github.com>
2022-03-21 10:25:15 -0600

    Further changes

--- advanceddemo/advanceddemo.ino
+++ advanceddemo/advanceddemo.ino
@@ -1,11 +1,56 @@
 void setup() 
 {
-  // Initialize the Camera
+  Serial.println("Ready... Set... Go!");
+  // SETTING UP SERIAL COMMUNICATION -----------------------------------
+  Serial.begin(115200);                         // Sets the data rate in bits per second (baud) for serial data transmission.
+
+  // PIN DEFINITIONS FOR AI THINKER -----------------------------------
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;   //PIXFORMAT_GRAYSCALE
+
+  Serial.println("Finished PIN Definitions.");
+  
+  // SELECTING IMAGE QUALITY BASED ON WHETHER "PSEUDO RAM" IS FOUND -----------------------------------
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;   // Highest Quality Selected; Alternatively: FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;             // 0-63, lower means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  Serial.println("Selected Image Quality Based on Whether PSEUDO RAM was Found");
+
+  // INITIALIZING THE CAMERA -----------------------------------
+  delay(250);     // Delay to ensure interface is up and running before we try to turn the camera on
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
-    return;
+  } else {
+    Serial.println("Initialized the Camera!");
   }
+  
 }
 
 void loop() {}
